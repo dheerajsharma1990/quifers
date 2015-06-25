45175b4cb4bd7c3c236c37e5fa7044627eb02c97
dheeraj <dheeraj.sharma@snapdeal.com>
2015-06-25 20:26:29 +0530

    add domain in copy jars script..

--- copyOnlyJarsToDev.sh
+++ copyOnlyJarsToDev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ./g.sh distZip
-scp -i quifers-dev-pk.pem web/build/libs/web.jar db-persister/build/libs/db-persister.jar ubuntu@52.74.237.216:~/artifacts/web/lib/
-scp -i quifers-dev-pk.pem email-util/build/libs/email-util.jar db-persister/build/libs/db-persister.jar ubuntu@52.74.237.216:~/artifacts/email-util/lib/
+scp -i quifers-dev-pk.pem web/build/libs/web.jar db-persister/build/libs/db-persister.jar domain/build/libs/domain.jar ubuntu@52.74.237.216:~/artifacts/web/lib/
+scp -i quifers-dev-pk.pem email-util/build/libs/email-util.jar db-persister/build/libs/db-persister.jar domain/build/libs/domain.jar ubuntu@52.74.237.216:~/artifacts/email-util/lib/
