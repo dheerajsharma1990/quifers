6972292aa9728142615f986d5f01c2101d2f771d
dheeraj <dheeraj.sharma@snapdeal.com>
2015-06-26 11:54:38 +0530

    logging changes...

--- db-persister/src/test/java/com/quifers/db/LocalDatabaseRunner.java
+++ db-persister/src/test/java/com/quifers/db/LocalDatabaseRunner.java
@@ -3,8 +3,10 @@ package com.quifers.db;
 import com.quifers.Environment;
 import com.quifers.properties.DBPersisterProperties;
 import com.quifers.properties.DBPersisterPropertiesLoader;
+import org.apache.log4j.PropertyConfigurator;
 import org.h2.tools.Server;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
@@ -16,7 +18,7 @@ public class LocalDatabaseRunner {
         server = Server.createTcpServer("-tcpPort", "9092");
         server.start();
         Class.forName(dbPersisterProperties.getDriverClass());
-        Connection connection = DriverManager.getConnection(dbPersisterProperties.getUrl(),dbPersisterProperties.getUserName(),dbPersisterProperties.getPassword());
+        Connection connection = DriverManager.getConnection(dbPersisterProperties.getUrl(), dbPersisterProperties.getUserName(), dbPersisterProperties.getPassword());
         connection.prepareStatement("DROP ALL OBJECTS").execute();
         SqlFilesSorter sqlFilesSorter = new SqlFilesSorter();
         SqlFilesExecutor executor = new SqlFilesExecutor(connection, sqlFilesSorter, new SqlScriptParser());
@@ -29,8 +31,14 @@ public class LocalDatabaseRunner {
     }
 
     public static void main(String[] args) throws Exception {
+        loadLog4jProperties(Environment.LOCAL);
         DBPersisterProperties dbPersisterProperties = DBPersisterPropertiesLoader.loadDbPersisterProperties(Environment.LOCAL);
         new LocalDatabaseRunner().runDatabaseServer(dbPersisterProperties);
+    }
+
+    private static void loadLog4jProperties(Environment environment) {
+        InputStream inputStream = LocalDatabaseRunner.class.getClassLoader().getResourceAsStream("properties/" + environment.name().toLowerCase() + "/log4j.properties");
+        PropertyConfigurator.configure(inputStream);
     }
 
 }
